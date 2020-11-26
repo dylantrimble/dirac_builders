1826bf10a0538251b5267d3a9e55b62df8cda617
dyl trimble <dyltrimble12@gmail.com>
2020-11-26 12:24:03 -0500

    added more to home page and set a grid for those components

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,18 @@
 console.log('running...')
+
+let home = () => {
+    console.log('home')
+}
+
+let portfolio = () => {
+    console.log('portfolio')
+}
+
+let contact = () => {
+    console.log('contact')
+}
+
+let about = () => {
+    console.log('about')
+}
+
